f4959af0edc2f232e76054e389c61d8e76028049
Your Name <kuberg8@gmail.com>
2020-06-27 20:47:03 +0500

    Updates

---
+++ precache-manifest.401ee5b906a4b65e4c8c3d4dde08cb5a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8b34548951a75507ed99ede3330c6a19",
+    "url": "/react2/index.html"
+  },
+  {
+    "revision": "2d0b751c3d1381bec03e",
+    "url": "/react2/static/css/main.63bcee22.chunk.css"
+  },
+  {
+    "revision": "dd589ca0013e0ccfb9a1",
+    "url": "/react2/static/js/2.4247a667.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react2/static/js/2.4247a667.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2d0b751c3d1381bec03e",
+    "url": "/react2/static/js/main.0cc36713.chunk.js"
+  },
+  {
+    "revision": "20036aa0631db9b71f75",
+    "url": "/react2/static/js/runtime-main.c6501f01.js"
+  },
+  {
+    "revision": "997055d96e9cfe5cf7ca3a214db532f8",
+    "url": "/react2/static/media/preloader.997055d9.svg"
+  }
+]);
